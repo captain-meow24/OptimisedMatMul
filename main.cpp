ea0d368ad8fc8d8812701add925b87c29773f190
Kanishka <kanishkasolanki456s@gmail.com>
2026-01-15 15:01:11 +0530

    made the matrices

--- main.cpp
+++ main.cpp
@@ -1,5 +1,19 @@
 #include <iostream>
+#include <vector>
 
 int main() {
+    std::vector<std::vector<int>> a, b,c,d;
+    for (int i=0; i<100; i++) {
+        std::vector<int> k;
+        for (int f=0; f<100; f++) {
+            k.push_back(1);
+        }
+        a.push_back(k);
+        std::vector<int> l;
+        for (int f=0; f<100; f++) {
+            l.push_back(2);
+        }
+        b.push_back(l);
+    }
     return 0;
 }
