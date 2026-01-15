345ff811ed919ae8f84f62201edf6358564ad9db
Kanishka <kanishkasolanki456s@gmail.com>
2026-01-16 00:28:30 +0530

    optimised to reduce cache miss

--- OptimisedMatMul.cpp
+++ OptimisedMatMul.cpp
@@ -1,3 +1,21 @@
 //
 // Created by kanishka on 12/1/26.
 //
+#include "OptimisedMatMul.h"
+#include <xmmintrin.h>
+#include <immintrin.h>
+
+void Optimised::cache_miss(std::vector<std::vector<int> > &a, std::vector<std::vector<int> > &b, std::vector<std::vector<int> > &c) {
+    for (int i=0; i<100; i++) {
+        for (int k=0; k<100; k++) {
+            for (int j=0;j<100;j++) {
+                c[i][j] += a[i][k] * b[k][j];
+            }
+        }
+    }
+}
+
+
+void Optimised::SSE(std::vector<std::vector<int> > &a, std::vector<std::vector<int> > &b, std::vector<std::vector<int> > &c) {
+
+}
--- OptimisedMatMul.h
+++ OptimisedMatMul.h
@@ -5,4 +5,14 @@
 #ifndef OPTIMISEDMATMUL_H
 #define OPTIMISEDMATMUL_H
 
+#include<iostream>
+#include<vector>
+
+class Optimised {
+    void cache_miss(std::vector<std::vector<int>> &a, std::vector<std::vector<int>> &b, std::vector<std::vector<int>> &c);
+    void SSE(std::vector<std::vector<int>> &a, std::vector<std::vector<int>> &b, std::vector<std::vector<int>> &c);
+    void AVX(std::vector<std::vector<int>> &a, std::vector<std::vector<int>> &b, std::vector<std::vector<int>> &c);
+};
+
+
 #endif //OPTIMISEDMATMUL_H
