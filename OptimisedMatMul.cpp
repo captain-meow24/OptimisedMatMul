02c0fbe1317bb087dcf56c5543712252db54ef4e
Kanishka Solanki <kanishkasolanki456s@gmail.com>
2026-01-26 03:15:32 +0530

    Add AVX function for optimized matrix multiplication
    Implemented AVX optimization for matrix multiplication.

--- OptimisedMatMul.cpp
+++ OptimisedMatMul.cpp
@@ -42,3 +42,22 @@ void Optimised::SSE(float (&a)[400][400],float (&b)[400] [400], float (&c)[400][
         }
     }
 }
+
+void Optimised::AVX(float (&a)[400][400], float (&b)[400][400], float (&c)[400][400]) {
+    alignas(32) float bt[400][400];
+    alignas(32) float temp[8];
+    transpose(b, bt, 400);
+    for (int i = 0; i < 400; i++) {
+        for (int j = 0; j < 400; j++) {
+            __m256 sum = _mm256_setzero_ps();
+            for (int k = 0; k + 7 < 400; k += 8) {
+                __m256 va = _mm256_loadu_ps(&a[i][k]);
+                __m256 vb = _mm256_loadu_ps(&bt[j][k]);
+                sum = _mm256_add_ps(sum, _mm256_mul_ps(va, vb));
+            }
+            _mm256_store_ps(temp, sum);
+            c[i][j] = temp[0] + temp[1] + temp[2] + temp[3] + temp[4] + temp[5] + temp[6] + temp[7];
+        }
+    }
+}
+
